f73b89028a1b90b23406d85da9fc5d63285f133e
rberneder <r_berneder@gmx.at>
2016-04-06 14:13:36 +0200

    # set-up tests

---
+++ saisonal-manager/dev/contacts/mock-contact.ts
@@ -0,0 +1,5 @@
+import {Contact} from "./contact";
+export const CONTACTS: Contact[] = [
+    {firstName: "Chris", lastName: "Ennser", phone: "1234", email: "asdf@asdf.at"},
+    {firstName: "Roland", lastName: "Berneder", phone: "1234", email: "asdf@asdf.at"},
+]
