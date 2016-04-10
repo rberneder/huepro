024c113574331c1668fa15fd5bb2c8520bf64c34
rberneder <r_berneder@gmx.at>
2016-04-10 20:50:57 +0200

    popular-products-area added

--- saisonal/dev/popular-products/popular-products.component.ts
+++ saisonal/dev/popular-products/popular-products.component.ts
@@ -1,21 +1,14 @@
-import { Component, ElementRef, OnInit } from "angular2/core";
-import { AnimationBuilder } from 'css-animator/builder';
-import { AnimationService } from 'css-animator/modules';
+import {Component} from "angular2/core";
+import {OnInit} from "angular2/core";
 
 @Component({
     selector: "popular-products",
     templateUrl: '/templates/popular-products/popular-products.template.html',
-    providers: [AnimationService],
 })
 export class PopularProductsComponent implements OnInit {
 
-    private animator: AnimationBuilder;
-
-    constructor (_animationService: AnimationService, private _elementRef: ElementRef) {
-        this.animator = _animationService.builder();
-    }
-
     ngOnInit():any {
-        this.animator.setType('fadeInUp').show(this._elementRef.nativeElement);
+        this.
+        console.log('Popular-Products loaded');
     }
 }
