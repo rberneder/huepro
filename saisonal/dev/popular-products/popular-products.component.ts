799ee272bbd20ee7504259596cc09a7eb5256307
rberneder <r_berneder@gmx.at>
2016-04-10 20:50:57 +0200

    popular-products-area added

---
+++ saisonal/dev/popular-products/popular-products.component.ts
@@ -0,0 +1,14 @@
+import {Component} from "angular2/core";
+import {OnInit} from "angular2/core";
+
+@Component({
+    selector: "popular-products",
+    templateUrl: '/templates/popular-products/popular-products.template.html',
+})
+export class PopularProductsComponent implements OnInit {
+
+    ngOnInit():any {
+        this.
+        console.log('Popular-Products loaded');
+    }
+}
