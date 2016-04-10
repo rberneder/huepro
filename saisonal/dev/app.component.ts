84e906905fe0f399629618913075e6b66f320f27
rberneder <r_berneder@gmx.at>
2016-04-10 20:50:57 +0200

    popular-products-area added

--- saisonal/dev/app.component.ts
+++ saisonal/dev/app.component.ts
@@ -3,11 +3,12 @@ import {ROUTER_DIRECTIVES} from "angular2/router";
 import {RouteConfig} from "angular2/router";
 import {SeasonCalendarComponent} from "./season-calendar/season-calendar.component";
 import {RecipesComponent} from "./recipes/recipes.component";
+import {PopularProductsComponent} from "./popular-products/popular-products.component";
 
 @Component({
     selector: 'app',
     templateUrl: '/templates/app.template.html',
-    directives: [SeasonCalendarComponent, ROUTER_DIRECTIVES]
+    directives: [SeasonCalendarComponent, ROUTER_DIRECTIVES, PopularProductsComponent]
 })
 @RouteConfig([
     {path: '/saisonkalender', name: 'SeasonCalendar', component: SeasonCalendarComponent},
