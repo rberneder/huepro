67694042997870776f3dce8b1848d37659529478
rberneder <r_berneder@gmx.at>
2016-04-12 15:38:11 +0200

    added product-component

--- saisonal/dev/season-calendar/season-calendar.component.ts
+++ saisonal/dev/season-calendar/season-calendar.component.ts
@@ -1,20 +1,25 @@
 import {Component} from "angular2/core";
 import {Router, RouteParams} from "angular2/router";
 import {OnInit} from "angular2/core";
+import {ProductService} from "../products/product.service";
+import {Product} from "../products/product";
 
 @Component({
     selector: "season-calendar",
     templateUrl: '/templates/season-calendar/season-calendar.template.html',
+    providers: [ProductService],
 })
 export class SeasonCalendarComponent implements OnInit {
     private month;
+    private products: Product[];
 
-    constructor(private _router: Router, private _routeParams: RouteParams) {
-        this.setMonth(_routeParams.get("month"));
+    constructor(private _productService: ProductService, private _router: Router, private _routeParams: RouteParams) {
+        this.setMonth(_routeParams.get('month'));
     };
     
     ngOnInit():any {
         console.log('Season-Calendar loaded for month: ' + this.month);
+        this.getContacts();
     }
     
     setMonth(month) {
@@ -26,5 +31,13 @@ export class SeasonCalendarComponent implements OnInit {
             console.warn('Invalid month param. Rerouting to month: ' + actMonth);
             this._router.navigate(['SeasonCalendar', {month: actMonth}]);
         }
+    }
+
+    getContacts() {
+        this._productService.getProducts().then((products: Product[]) => this.products = products);
+    }
+
+    testFunction(id) {
+        console.log(this._productService.getProduct(id)._result);
     }
 }
