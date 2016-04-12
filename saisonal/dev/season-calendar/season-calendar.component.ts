07596b2317e46c71a2c638cc8aa0582b08435b6e
rberneder <r_berneder@gmx.at>
2016-04-12 17:45:08 +0200

    click through month enabled

--- saisonal/dev/season-calendar/season-calendar.component.ts
+++ saisonal/dev/season-calendar/season-calendar.component.ts
@@ -3,6 +3,7 @@ import {Router, RouteParams} from "angular2/router";
 import {OnInit} from "angular2/core";
 import {ProductService} from "../products/product.service";
 import {Product} from "../products/product";
+import {MONTHS} from "../util/month.seed";
 
 @Component({
     selector: "season-calendar",
@@ -10,22 +11,27 @@ import {Product} from "../products/product";
     providers: [ProductService],
 })
 export class SeasonCalendarComponent implements OnInit {
-    private month;
+    private actMonth;
+    private prevMonth;
+    private nextMonth;
+    private monthNames;
     private products: Product[];
 
     constructor(private _productService: ProductService, private _router: Router, private _routeParams: RouteParams) {
         this.setMonth(_routeParams.get('month'));
+        this.monthNames = MONTHS;
     };
     
     ngOnInit():any {
-        console.log('Season-Calendar loaded for month: ' + this.month);
         this.getContacts();
     }
     
     setMonth(month) {
         let monthNr = parseInt(month);
         if (monthNr >= 0 && monthNr <= 11) {
-            this.month = monthNr;
+            this.actMonth = monthNr;
+            this.prevMonth = (this.actMonth - 1 < 0) ? 11 : (this.actMonth - 1);
+            this.nextMonth = (this.actMonth + 1) % 12;
         } else {
             let actMonth = new Date().getMonth();
             console.warn('Invalid month param. Rerouting to month: ' + actMonth);
@@ -37,7 +43,15 @@ export class SeasonCalendarComponent implements OnInit {
         this._productService.getProducts().then((products: Product[]) => this.products = products);
     }
 
+    gotoPrevMonth() {
+        this._router.navigate(['SeasonCalendar', {month: this.prevMonth}]);
+    }
+
+    gotoNextMonth() {
+        this._router.navigate(['SeasonCalendar', {month: this.nextMonth}]);
+    }
+
     testFunction(id) {
-        console.log(this._productService.getProduct(id)._result);
+        console.log(this._productService.getProduct(id));
     }
 }
