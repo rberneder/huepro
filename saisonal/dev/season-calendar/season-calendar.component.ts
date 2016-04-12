6bd2deb2eae8801623ff8bc7feb7efab1e12bd30
rberneder <r_berneder@gmx.at>
2016-04-12 11:21:56 +0200

    season-calendar routing now with month as param

--- saisonal/dev/season-calendar/season-calendar.component.ts
+++ saisonal/dev/season-calendar/season-calendar.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "angular2/core";
+import {Router, RouteParams} from "angular2/router";
 import {OnInit} from "angular2/core";
 
 @Component({
@@ -6,8 +7,24 @@ import {OnInit} from "angular2/core";
     templateUrl: '/templates/season-calendar/season-calendar.template.html',
 })
 export class SeasonCalendarComponent implements OnInit {
+    private month;
 
+    constructor(private _router: Router, private _routeParams: RouteParams) {
+        this.setMonth(_routeParams.get("month"));
+    };
+    
     ngOnInit():any {
-        console.log('Season-Calendar loaded');
+        console.log('Season-Calendar loaded for month: ' + this.month);
+    }
+    
+    setMonth(month) {
+        let monthNr = parseInt(month);
+        if (monthNr >= 0 && monthNr <= 11) {
+            this.month = monthNr;
+        } else {
+            let actMonth = new Date().getMonth();
+            console.warn('Invalid month param. Rerouting to month: ' + actMonth);
+            this._router.navigate(['SeasonCalendar', {month: actMonth}]);
+        }
     }
 }
