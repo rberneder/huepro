7e8dbdeea2b27b8fef186fea83cd07245669d6a6
rberneder <r_berneder@gmx.at>
2016-04-14 17:26:07 +0200

    http-test added

--- saisonal/dev/products/product-list.component.ts
+++ saisonal/dev/products/product-list.component.ts
@@ -3,7 +3,7 @@ import { ROUTER_DIRECTIVES } from 'angular2/router';
 
 @Component({
     selector: "product-list",
-    templateUrl: ['/templates/products/product-list.template.html'],
+    templateUrl: '/templates/products/product-list.template.html',
     directives: [ROUTER_DIRECTIVES]
 })
 export class ProductListComponent implements OnInit {
