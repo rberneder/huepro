b42907e4fedb8ec6f503084d7bae907b88c1b7ff
rberneder <r_berneder@gmx.at>
2016-04-17 10:11:50 +0200

    removed route-files (are defined in app.js)

--- saisonal/routes/frontEnd.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
-});
-
-module.exports = router;
