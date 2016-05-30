3b62336fe534aa718dfff776cb5185f8ef7cef70
rberneder <r_berneder@gmx.at>
2016-05-30 14:52:56 +0200

    changed thumbnail-width to 400px

--- saisonal-api/util/imageGenerator.js
+++ saisonal-api/util/imageGenerator.js
@@ -4,7 +4,7 @@ var gm = require('gm').subClass({imageMagick: true});
 
 exports.createThumb = function(imgUrl) {
     var imgThumb = imgUrl.substr(0, (imgUrl.length - 4)) + '_thumb' + imgUrl.substr(-4);
-    gm(imgUrl).resize('300').write(imgThumb, function (err) {
+    gm(imgUrl).resize('400').write(imgThumb, function (err) {
         if (err) console.log(err);
     });
 }
