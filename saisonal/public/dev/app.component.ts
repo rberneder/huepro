e61697bf0155a279a3bfab32fd065261572b53a1
Roland <r_berneder@gmx.at>
2016-11-21 11:08:42 +0100

    update

--- saisonal/public/dev/app.component.ts
+++ saisonal/public/dev/app.component.ts
@@ -1,33 +1,41 @@
-import {Component, ElementRef} from 'angular2/core';
-import {ROUTER_DIRECTIVES, Router, RouteConfig} from "angular2/router";
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
+
+import { AppComponent }         from './app.component';
 import {FreshProductsComponent} from "./fresh-products/fresh-products.component";
-import {ProductService} from "./products/product.service";
-import {SearchComponent} from "./search/search.component";
 import {ProductContainerComponent} from "./products/product-container.component";
-import {ScrollService} from "./util/scroll/scroll.service";
 import {SeasonCalendarContainerComponent} from "./season-calendar/season-calendar-container.component";
+import {RecipeContainerComponent} from "./recipes/recipe-container.component";
+import {SearchComponent} from "./search/search.component";
 import {ContactComponent} from "./contact/contact.component";
 import {ImprintComponent} from "./imprint/imprint.component";
-import {RecipeContainerComponent} from "./recipes/recipe-container.component";
+import {ProductService} from "./products/product.service";
+import {ScrollService} from "./util/scroll/scroll.service";
 
-
-
-@Component({
-    selector: 'app',
-    templateUrl: '/templates/app.template.html',
-    providers: [ProductService],
-    directives: [ROUTER_DIRECTIVES],
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule,
+        RouterModule.forRoot([
+            {path: '/', name: 'FreshProducts', component: FreshProductsComponent, useAsDefault: true},
+            {path: '/produkte/...', name: 'Products', component: ProductContainerComponent},
+            {path: '/saisonkalender/...', name: 'SeasonCalendar', component: SeasonCalendarContainerComponent},
+            {path: '/rezepte/...', name: 'Recipes', component: RecipeContainerComponent},
+            {path: '/suche', name: 'Search', component: SearchComponent},
+            {path: '/kontakt', name: 'Contact', component: ContactComponent},
+            {path: '/impressum', name: 'Imprint', component: ImprintComponent}
+        ])
+    ],
+    declarations: [
+        AppComponent,
+        HeroListComponent,
+        CrisisListComponent
+    ],
+    bootstrap: [ AppComponent ]
 })
-@RouteConfig([
-    {path: '/', name: 'FreshProducts', component: FreshProductsComponent, useAsDefault: true},
-    {path: '/produkte/...', name: 'Products', component: ProductContainerComponent},
-    {path: '/saisonkalender/...', name: 'SeasonCalendar', component: SeasonCalendarContainerComponent},
-    {path: '/rezepte/...', name: 'Recipes', component: RecipeContainerComponent},
-    {path: '/suche', name: 'Search', component: SearchComponent},
-    {path: '/kontakt', name: 'Contact', component: ContactComponent},
-    {path: '/impressum', name: 'Imprint', component: ImprintComponent},
-])
-export class AppComponent {
+export class AppModule {
 
     private month;
     private mobileMenuActive: boolean;
@@ -42,8 +50,21 @@ export class AppComponent {
             this.toggleMobileNav();
         }
     }
-    
+
     toggleMobileNav() {
         this.mobileMenuActive = !this.mobileMenuActive;
     }
 }
+
+
+
+
+
+
+
+
+@Component({
+    selector: 'app',
+    templateUrl: '/templates/app.template.html',
+    providers: [ProductService]
+})
